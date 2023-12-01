3329e2aca1f3391011f02c2eb48aebdf508fcb17
husanjonmusayev <husanjonm279@gmail.com>
2023-12-01 12:56:41 +0500

    uy ishi

---
+++ script.js
@@ -0,0 +1,9 @@
+let n = +prompt("safar uchun toplangan summani kiriting")
+let dollor =750*1100.34
+let evro =120*12354.03
+let sum = dollor+evro
+if(n>sum){
+    console.log("oq yo'l, Alisher!");
+}else{
+    console.log("Alisher,ozigina sabir qilish kerak bolar ekan");
+}
